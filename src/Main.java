d192b6049c4f40b3a239964c85ded539fb7ebd07
dopal2 <jeongdoyun9510@gmail.com>
2024-02-21 17:09:13 +0900

    update

--- src/Main.java
+++
@@ -1,7 +0,0 @@
-import java.util.*;
-public class Main {
-    public static void main(String[] arg) throws Exception{
-        Scanner sc = new Scanner(System.in);
-        int a = sc.nextInt();
-    }
-}
