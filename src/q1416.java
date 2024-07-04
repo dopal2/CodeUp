9e057a771834a46280bcdf0372efd78dbcd8e883
dopal2 <jeongdoyun9510@gmail.com>
2024-07-04 17:46:26 +0900

    update

---
+++ src/q1416.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class q1416 {
+    public static void main(String[] arg) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        System.out.println(Integer.toBinaryString(num));
+    }
+}
