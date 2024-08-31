3674c26a3e3e2fb8a1b3a704a260fe6558ec45a1
dopal2 <jeongdoyun9510@gmail.com>
2024-08-31 17:11:26 +0900

    update

---
+++ src/q1406.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class q1406{
+    public static void main(String[] arg) throws Exception{
+        Scanner sc = new Scanner(System.in);
+        String str = sc.next();
+        if(str.indexOf("love") > -1) System.out.println("I love you.");
+    }
+}
